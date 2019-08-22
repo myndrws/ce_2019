e7509004e43569c3f131489ac5f63fd391a861b2
myndrws <myndrws@gmail.com>
2019-08-22 21:29:54 +0100

    Created a word frequency graph function

--- report_reader.R
+++ report_reader.R
@@ -11,13 +11,16 @@ library(tidyverse) # for general data manipulation
 library(testthat) # for checking the code along the way
 library(officer) # for reading the styles of the documents
 library(data.table) # for using quick things
+library(ggplot2) # for graphing
+library(plotly) # for interactive graphing
+library(tidytext) # for word frequencies
 
 # read in word document as one long string
 report <- readtext::readtext("C:/Users/Amy/Documents/ce_report_example.docx")
 # any checks that the read-in document should have?
 
 report <- as.data.frame(report) %>%
-  mutate(report = as.character(report))
+  mutate(text = as.character(text))
 styles_info(report)
 
 # # this will read in the report with line breaks and so multiple string records
@@ -27,50 +30,57 @@ styles_info(report)
 # # we can see styles info from the doc read in with read_docx
 # # but still can't get the style info of our document
 # styles_info(report2)
-# 
-# 
-# report2 %>% head()
+
 report %>% head()
 
 # locating parts of report with regular expressions
 
 # title of report
-title_of_report <- report$report[1]
+title_of_report <- report$text[1]
 
 # important sections of report
   # Search for the Abstract:
-location_first_section <- grep("new partners/ organisations?", report$report)[1]
+location_first_section <- grep("new partners/ organisations?", report$text)[1]
 
 # Search for the Keyword
-location_second_section <- grep("this is a new heading", report$report)[1]
+location_second_section <- grep("this is a new heading", report$text)[1]
 
 # The text in between these 2 sections will be the text we want
-first_section_text <- paste(report$report[(location_first_section+1):(location_second_section-1)], collapse=" ")
+section_text <- paste(report$text[(location_first_section+1):(location_second_section-1)], collapse=" ")
 
-# word frequency data and plotly graph
-library(ggplot2)
-library(plotly)
-library(tidytext)
+first_section <- data.frame(first_section_text) %>%
+  mutate(first_section_text = as.character(first_section_text))
 
-# split into words - NEEDS EDITING FOR SECTIONS NOT WHOLE REPORT
-section_by_words <- report %>%
-  unnest_tokens(word, report)
 
-# find document-word counts & remove stopwords
-word_counts <- by_question_word %>%
+# create function to split the words of the section and plot them
+
+word_frequencies <- function(section_df, text_var) {
+  
+section_df <- section_df %>%
+  rowid_to_column()
+# split just that section into words &
+# find document-word counts & 
+# remove stopwords
+word_counts <- section_df %>%
+  unnest_tokens(word, !!text_var) %>%
   anti_join(stop_words) %>%
-  count(id, word, sort = TRUE) %>%
-  ungroup()
+  count(rowid, word, sort = TRUE) %>%
+  ungroup() %>%
+  filter(n > 2) # where the wordcount is greater than one
 
 # visualise the top 5 words in these topics 
-top_terms %>%
-  mutate(term = reorder(term, beta)) %>%
-  ggplot(aes(term, beta, fill = factor(topic))) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~ topic, scales = "free") +
+p <- ggplotly(
+  word_counts %>%
+    ggplot(aes(reorder(word, n), n, fill = as.factor(word))) +
+    geom_col(show.legend = FALSE) +
+    theme(legend.position = "none", axis.title.y = element_blank()) 
+    ggtitle("Count of most frequent words in section") +
   coord_flip()
+)
 
-# counting from sections of data 
+return(p)
 
+}
 
+word_frequencies(first_section, "first_section_text")
 
