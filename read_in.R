8de38465ee9cee670ceb0d9241dacbcf3b61a5cc
myndrws <myndrws@gmail.com>
2019-09-24 19:00:12 +0100

    function to read in word or pdf

---
+++ read_in.R
@@ -0,0 +1,42 @@
+# function to read in either pdf or word documents
+
+# this still needs to get the word and pdf document into the same format at the end
+
+require(testthat)
+require(tidyverse)
+require(pdftools)
+require(readtext)
+
+read_in <- function(input_file) {
+  
+  testthat::expect_type(input_file, "character")
+  
+  is_word <- grepl(".docx", input_file)
+  is_pdf <- grepl(".pdf", input_file)
+  
+  if (is_word == TRUE & is_pdf == FALSE) {
+    
+    # read in word document
+    t <- readtext::readtext(input_file) %>%
+      as.data.frame() %>%
+      mutate(text = as.character(text))
+    
+    return(t)
+    
+  } else if (is_pdf == TRUE & is_word == FALSE) {
+    
+    # read in pdf document
+    t <- pdftools::pdf_text(input_file) %>%
+      as.data.frame() %>%
+      mutate(text = as.character(`.`)) %>%
+      select(-`.`)
+    
+    return(t)
+    
+  } else {
+    
+    stop("Unrecognised file input: this should be a docx or pdf")
+    
+  }
+  
+}
