87318f732882ad321c917fb3bc734ccdcce961ca
EW-commits <54326448+EW-commits@users.noreply.github.com>
2019-10-07 18:42:26 +0100

    Pull from master to Emma (#2)
    * function to read in word or pdf
    
    * updated read_in to produce consistent output from docx or pdfs
    
    * Added a dummy data report

---
+++ read_in.R
@@ -0,0 +1,46 @@
+# function to read in either pdf or word documents
+
+# this still needs to get the word and pdf document into the same format at the end
+
+require(testthat)
+require(tidyverse)
+require(pdftools)
+require(readtext)
+
+read_in <- function(input_file) {
+  
+  testthat::expect_type(input_file, "character")
+  
+  is_word <- grepl(".docx", input_file)
+  is_pdf <- grepl(".pdf", input_file)
+  
+  if (is_word == TRUE & is_pdf == FALSE) {
+    
+    # read in word document
+    t <- readtext::readtext(input_file) %>%
+      as.data.frame() %>%
+      mutate(text = as.character(text)) %>%
+      select(-doc_id)
+    
+    return(t)
+    
+  } else if (is_pdf == TRUE & is_word == FALSE) {
+    
+    # read in pdf document
+    t <- pdftools::pdf_text(input_file) %>%
+      as.data.frame() %>%
+      mutate(text = as.character(`.`)) %>%
+      select(-`.`)
+    
+    # note: this includes the tags of the text 
+    # BUT further analysis seems to ignore these
+    
+    return(t)
+    
+  } else {
+    
+    stop("Unrecognised file input: this should be a docx or pdf")
+    
+  }
+  
+}
