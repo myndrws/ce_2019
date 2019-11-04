69ce96854c0f2760f4045dcf198711eed1980e23
myndrws <myndrws@gmail.com>
2019-11-04 16:53:57 +0000

    added bind_tokenise function

--- read_in.R
+++ read_in.R
@@ -1,9 +1,10 @@
 #--------------------------------------------------------------------#
-# Program purpose: function to read in either pdf or word documents  #
+# Program purpose: functiona to read in either pdf or word documents #
 # Author: Amy Andrews                                                #
 # Date: OCT2019                                                      #
 #--------------------------------------------------------------------#
-# this still needs to get the word and pdf document into the same format at the end
+
+# 2 functions: read in and bind together
 
 library(testthat)
 library(pdftools)
@@ -34,16 +35,46 @@ read_in <- function(input_file) {
       mutate(text = as.character(`.`)) %>%
       select(-`.`)
     
-    # note: this includes the tags of the text 
-    # BUT further analysis seems to ignore these
-    
     return(t)
     
   } else {
     
     warning("Unrecognised file input: this should be a docx or pdf")
-    #stop("Unrecognised file input: this should be a docx or pdf")
     
   }
   
 }
+
+
+# create function to bind together the rows read in where needed
+# and tokenise these 
+
+bind_tokenise <- function(data, report_id_col) {
+
+# iterate through each report - linking all strings in a file into one big string
+for(i in 1:max(reports_id_col)){
+  
+  # select one report at a time
+  report_selected <- data %>% filter(report_id_col == i )
+  
+  # create one string
+  report_string <- paste(report_selected[,"text"])
+  
+  # split into sentences
+  report_tokenized <- unlist(tokenize_sentences(report_string))
+  
+  # create data frame with sentence as each row and sentence Id
+  report_analysis <- as.data.frame(report_tokenized) %>%
+    mutate(sentence_id = row_number(), report_id = i) 
+  
+  if(exists("report_tokenized_all")){
+    report_tokenized_all <<- rbind(report_analysis, report_tokenized_all)
+  }else{
+    report_tokenized_all <<- report_analysis
+  }
+}
+
+
+}
+
+
