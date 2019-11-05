b8ecb5fbc8587ca3e7f3b221eabd06229a0f57fd
myndrws <myndrws@gmail.com>
2019-11-05 10:45:21 +0000

    read_in has url path, only one function in file

--- functions/read_in.R
+++ functions/read_in.R
@@ -1,12 +1,12 @@
 #--------------------------------------------------------------------#
-# Program purpose: functiona to read in either pdf or word documents #
+# Program purpose: functions to read in either pdf or word documents #
 # Author: Amy Andrews                                                #
 # Date: OCT2019                                                      #
 #--------------------------------------------------------------------#
 
-# 2 functions: read in and bind together
 
 library(testthat)
+library(tidyverse)
 library(pdftools)
 library(readtext)
 
@@ -14,7 +14,7 @@ read_in <- function(input_file) {
   
   testthat::expect_type(input_file, "character")
   
-  is_word <- grepl(".docx", input_file)
+  is_word <- (grepl(".docx", input_file) | grepl(".docx", input_file)) #checks for all word docs
   is_pdf <- grepl(".pdf", input_file)
   
   if (is_word == TRUE & is_pdf == FALSE) {
@@ -23,7 +23,12 @@ read_in <- function(input_file) {
     t <- readtext::readtext(input_file) %>%
       as.data.frame() %>%
       mutate(text = as.character(text)) %>%
-      select(-doc_id)
+      select(-doc_id) %>%
+      # remove numbers
+      mutate(text = map(text, function(x) gsub("\\d+", "", x))) %>%
+      # remove the pdf characters
+      mutate(text = map(text, function(x) gsub("\\n|\\b|\\r", "", x))) %>%
+      mutate(filepath = input_file)
     
     return(t)
     
@@ -33,7 +38,12 @@ read_in <- function(input_file) {
     t <- pdftools::pdf_text(input_file) %>%
       as.data.frame() %>%
       mutate(text = as.character(`.`)) %>%
-      select(-`.`)
+      select(-`.`) %>%
+      # remove numbers
+      mutate(text = map(text, function(x) gsub("\\d+", "", x))) %>%
+      # remove the pdf characters
+      mutate(text = map(text, function(x) gsub("\\n|\\b|\\r", "", x))) %>%
+      mutate(filepath = input_file)
     
     return(t)
     
@@ -43,38 +53,7 @@ read_in <- function(input_file) {
     
   }
   
-}
-
-
-# create function to bind together the rows read in where needed
-# and tokenise these 
-
-bind_tokenise <- function(data, report_id_col) {
-
-# iterate through each report - linking all strings in a file into one big string
-for(i in 1:max(reports_id_col)){
   
-  # select one report at a time
-  report_selected <- data %>% filter(report_id_col == i )
-  
-  # create one string
-  report_string <- paste(report_selected[,"text"])
-  
-  # split into sentences
-  report_tokenized <- unlist(tokenize_sentences(report_string))
-  
-  # create data frame with sentence as each row and sentence Id
-  report_analysis <- as.data.frame(report_tokenized) %>%
-    mutate(sentence_id = row_number(), report_id = i) 
-  
-  if(exists("report_tokenized_all")){
-    report_tokenized_all <<- rbind(report_analysis, report_tokenized_all)
-  }else{
-    report_tokenized_all <<- report_analysis
-  }
-}
-
-
 }
 
 
