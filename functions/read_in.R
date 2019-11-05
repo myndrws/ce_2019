47e2333023a5489ced0bdf492bc80ae0181f536b
myndrws <myndrws@gmail.com>
2019-11-05 09:43:35 +0000

    replace the read in file so in one place

--- functions/read_in.R
+++ functions/read_in.R
@@ -1,9 +1,12 @@
-# function to read in either pdf or word documents
+#--------------------------------------------------------------------#
+# Program purpose: functiona to read in either pdf or word documents #
+# Author: Amy Andrews                                                #
+# Date: OCT2019                                                      #
+#--------------------------------------------------------------------#
 
-# this still needs to get the word and pdf document into the same format at the end
+# 2 functions: read in and bind together
 
 library(testthat)
-library(tidyverse)
 library(pdftools)
 library(readtext)
 
@@ -20,11 +23,7 @@ read_in <- function(input_file) {
     t <- readtext::readtext(input_file) %>%
       as.data.frame() %>%
       mutate(text = as.character(text)) %>%
-      select(-doc_id) %>%
-      # remove numbers
-      mutate(text = map(text, function(x) gsub("\\d+", "", x))) %>%
-      # remove the pdf characters
-      mutate(text = map(text, function(x) gsub("\\n|\\b|\\r", "", x)))
+      select(-doc_id)
     
     return(t)
     
@@ -34,14 +33,7 @@ read_in <- function(input_file) {
     t <- pdftools::pdf_text(input_file) %>%
       as.data.frame() %>%
       mutate(text = as.character(`.`)) %>%
-      select(-`.`) %>%
-      # remove numbers
-      mutate(text = map(text, function(x) gsub("\\d+", "", x))) %>%
-      # remove the pdf characters
-      mutate(text = map(text, function(x) gsub("\\n|\\b|\\r", "", x)))
-    
-    # note: this includes the tags of the text 
-    # BUT further analysis seems to ignore these
+      select(-`.`)
     
     return(t)
     
@@ -52,3 +44,37 @@ read_in <- function(input_file) {
   }
   
 }
+
+
+# create function to bind together the rows read in where needed
+# and tokenise these 
+
+bind_tokenise <- function(data, report_id_col) {
+
+# iterate through each report - linking all strings in a file into one big string
+for(i in 1:max(reports_id_col)){
+  
+  # select one report at a time
+  report_selected <- data %>% filter(report_id_col == i )
+  
+  # create one string
+  report_string <- paste(report_selected[,"text"])
+  
+  # split into sentences
+  report_tokenized <- unlist(tokenize_sentences(report_string))
+  
+  # create data frame with sentence as each row and sentence Id
+  report_analysis <- as.data.frame(report_tokenized) %>%
+    mutate(sentence_id = row_number(), report_id = i) 
+  
+  if(exists("report_tokenized_all")){
+    report_tokenized_all <<- rbind(report_analysis, report_tokenized_all)
+  }else{
+    report_tokenized_all <<- report_analysis
+  }
+}
+
+
+}
+
+
